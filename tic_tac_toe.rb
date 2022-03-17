6c29579e0ee5a4cf763dcee04df96d7d9dd5dde3
JustAida <suradet40@gmail.com>
2022-03-17 08:23:39 +0700

    Create pseudocode

---
+++ tic_tac_toe.rb
@@ -0,0 +1,16 @@
+# Board template. [Use "." as empty spaces.]
+# |.|.|.|
+# |.|.|.|
+# |.|.|.|
+
+# Create "Board" class.
+# Create a "display" instance variable to show the current state of the board.
+# Create a method to place "X" or "O" on the board.
+# Create a method to check if there is a winner.
+
+# Loop the game until there is a winner.
+# Ask the player the location to place "X" or "O" on the board.
+# Display the board.
+# Switch the turn of the player.
+
+# A message to declare the winner.
